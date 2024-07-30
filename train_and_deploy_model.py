65bbc82b5de4310a25e5e2bb1bdefc41596d684e
Ángel Gustavo José Martínez <141785336+angujoma@users.noreply.github.com>
2024-07-30 16:12:44 -0600

    Update train_and_deploy_model.py

--- train_and_deploy_model.py
+++ train_and_deploy_model.py
@@ -21,7 +21,7 @@ X = df_transformed.drop(columns=['TARGET'])
 y = df_transformed['TARGET']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Entrenar el modelo de regresión logística
+# Entrenar el modelo de regresión logística (baseline)
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
